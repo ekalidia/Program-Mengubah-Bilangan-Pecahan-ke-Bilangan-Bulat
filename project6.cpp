c35557473a0f03840daf3fab72e3e6f1f6da6445
Eka Lidia Wati <45529774+ekalidia@users.noreply.github.com>
2018-12-13 11:56:45 +0700

    Add files via upload

---
+++ project6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+    int a;
+    cout<< " Program Mengubah Bilangan Pecahan menjadi Bilangan Bulat "<<endl;
+    cout<<endl;
+    cout<< " Masukkan bilangan Pecahan = ";
+    cin>> a;
+    cout<<endl;
+    cout<< " Maka hasil Bilangan Bulatnya adalah = "<< a <<endl;
+
+    getch();
+}
